d653561a50c98d26085a664a99db21c2686b2ce6
Bbylumi <odetoyinbopelumi42@gmail.com>
2024-11-21 11:57:30 +0100

    report model

--- src/models/report.model.ts
+++ src/models/report.model.ts
@@ -2,5 +2,26 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IReport extends Document {
     month: string;
-    year: string
+    year: string;
+    attendance: {
+        sunday: number;
+        midweek: {
+            Tuesday: number;
+            Thursday: number;
+        }
+    }
 } 
+
+const ReportSchema: Schema = new Schema({
+    month: { type: String, required: true },
+    year: { type: String, required: true },
+    attendance: {
+        sunday: { type: String, default: 0 },
+        midweek: {
+            Tuesday: { type: Number, default: 0},
+            Thursday: { type: Number, default: 0}
+        },
+    },
+});
+
+export default mongoose.model<IReport>("Report", ReportSchema)
