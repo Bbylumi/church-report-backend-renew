f803ddbd8c1bdb7ea5f14cfc2c2dcea3e8ec3b1f
Bbylumi <odetoyinbopelumi42@gmail.com>
2024-11-25 13:47:34 +0100

    implement the API endpoint and map them to their respective controllers

---
+++ src/routes/monetary.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import monetaryControllers from "../controllers/monetary.controllers";
+
+const router = Router()
+
+router.post("/", monetaryControllers.createMonetary);
+router.get("/", monetaryControllers.getMonetary);
+router.get("/:id", monetaryControllers.getMonetaryById);
+router.put("/:id", monetaryControllers.updateMonetary);
+router.delete("/:id", monetaryControllers.deleteMonetary)
+
+export default router;
